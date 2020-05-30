1668dbf32fd019fe4a79ba8cec3a83986d4793ff
BloodyWolf233 <BloodyWolf233@users.noreply.github.com>
2020-05-30 17:43:31 +0800

    3

--- manager.py
+++ manager.py
@@ -18,10 +18,10 @@ def init_database_test():
     db.create_all()
     for i in range(0, 100):
         db.session.add(User('User' + str(i), 'a' + str(i)))
-        for j in range(0, 3):
+        for j in range(0, 5):
             db.session.add(Image(get_image_url(), i + 1))
             for k in range(0, 3):
-                db.session.add(Comment('This is a comment' + str(k), 1 + 3 * i + j, i + 1))
+                db.session.add(Comment('This is a comment' + str(k), 1 + 5 * i + j, i + 1))
     db.session.commit()
 
 
